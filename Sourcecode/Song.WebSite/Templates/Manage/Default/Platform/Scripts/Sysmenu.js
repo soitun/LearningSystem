9a31470dda023ba0cae0d933f2aea7005a03dbdc
weishakeji <10522779@qq.com>
2025-09-01 10:31:26 +0800

    修正细节

--- Sourcecode/Song.WebSite/Templates/Manage/Default/Platform/Scripts/Sysmenu.js
+++ Sourcecode/Song.WebSite/Templates/Manage/Default/Platform/Scripts/Sysmenu.js
@@ -1,6 +1,6 @@
 $ready(function () {
     window.vapp = new Vue({
-        el: '#app',
+        el: '#vapp',
         data: {
             data: [],
             curr: {},    //当前要编辑的节点          
@@ -115,13 +115,14 @@ $ready(function () {
                 }).catch(function () {
                 });
             },
+            //保存菜单项
             updateSave: function () {
                 var th = this;
                 th.loading = true;
                 $api.post('ManageMenu/SystemMenuUpdate', { 'tree': th.data }).then(function (req) {
                     if (req.data.success) {
                         var result = req.data.result;
-                        th.$notify({
+                        th.$message({
                             type: 'success',
                             message: '系统菜单保存成功！',
                             position: 'bottom-left',
