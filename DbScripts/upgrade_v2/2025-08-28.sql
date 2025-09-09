b0fc0ee615108e906b650f2177b3eaceeb51b8b1
weishakeji <10522779@qq.com>
2025-09-09 21:42:46 +0800

    修订升级脚本

--- DbScripts/upgrade_v2/2025-08-28.sql
+++ DbScripts/upgrade_v2/2025-08-28.sql
@@ -21,8 +21,10 @@ ALTER TABLE IF EXISTS "TestPaperItem" DROP COLUMN IF EXISTS "Tp_UID" CASCADE;
 ALTER TABLE  IF EXISTS "TestPaperItem" ADD COLUMN "Tp_Id" int8 NOT NULL  DEFAULT 0;
 
 /*修订，增加人工批阅的字段*/
-ALTER TABLE IF EXISTS  "Examination" ADD COLUMN "Exam_IsManual" BOOLEAN  NOT NULL DEFAULT true;
-ALTER TABLE IF EXISTS  "TestPaper" ADD COLUMN "Tp_IsManual" BOOLEAN  NOT NULL DEFAULT true;
+ALTER TABLE IF EXISTS  "Examination" ADD COLUMN "Exam_IsManual" BOOLEAN  NOT NULL DEFAULT FALSE;
+ALTER TABLE IF EXISTS  "TestPaper" ADD COLUMN "Tp_IsManual" BOOLEAN  NOT NULL DEFAULT FALSE;
+CREATE INDEX IF NOT EXISTS "Examination_Exam_IsManual" ON "Examination" ("Exam_IsManual" ASC);
+CREATE INDEX IF NOT EXISTS "TestPaper_Tp_IsManual" ON "TestPaper" ("Tp_IsManual" ASC);
 
 /*修订一些其它字段，也许不用修订*/
 UPDATE "SingleSignOn" set "SSO_IsAddSort"=FALSE WHERE "SSO_IsAddSort" ISNULL;
@@ -30,10 +32,12 @@ ALTER TABLE "SingleSignOn" ALTER COLUMN "SSO_IsAddSort" SET NOT NULL;
 UPDATE "TestResults" set "Tr_Score"=0 WHERE "Tr_Score" ISNULL;
 ALTER TABLE "TestResults" ALTER COLUMN "Tr_Score" SET NOT NULL;
 
+
 /*有简答题的试卷，以及引用该试卷的考试，设置人工批阅*/
 --SELECT "Tp_Id" FROM "TestPaper" WHERE (xpath('/Config/All/Items/TestPaperItem/TPI_Count/text()', "Tp_FromConfig"::xml ))[4]::text!='0';
 --SELECT "Tp_Id" FROM "TestPaper" WHERE (xpath('/Config/Outline/Percent/TestPaperItem/TPI_Count/text()', "Tp_FromConfig"::xml ))[4]::text!='0';
-
+UPDATE "TestPaper" set "Tp_IsManual"=FALSE;
+UPDATE "Examination" set "Exam_IsManual"=FALSE;
 UPDATE "TestPaper" set "Tp_IsManual"=true  WHERE (xpath('/Config/All/Items/TestPaperItem/TPI_Count/text()', "Tp_FromConfig"::xml ))[4]::text!='0';
 UPDATE "TestPaper" set "Tp_IsManual"=true  WHERE (xpath('/Config/Outline/Percent/TestPaperItem/TPI_Count/text()', "Tp_FromConfig"::xml ))[4]::text!='0';
 
