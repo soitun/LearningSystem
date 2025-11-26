032135d020204f912f6334682df7f301ce4a0140
weishakeji <10522779@qq.com>
2025-11-26 11:51:29 +0800

    编写试卷编辑的跳转页

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Scripts/Modify.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Scripts/Modify.js
@@ -3,6 +3,9 @@ $ready(function () {
         el: '#vapp',
         data: {
             id: $api.querystring('id'), //主键ID
+            testpaper: {},
+            form: {},
+            //加载状态
             loadstate: {
                 init: false,        //初始化
                 def: false,         //默认
@@ -12,7 +15,7 @@ $ready(function () {
             }
         },
         mounted: function () {
-
+            this.getentity();
         },
         created: function () {
 
@@ -30,29 +33,59 @@ $ready(function () {
             },
             //是否为新增
             isadd: t => t.id == null || t.id == '' || this.id == 0,
+            //试卷对象是否为空
+            isnull: t => $api.isnull(t.testpaper),
         },
         watch: {
+            //选择时间区间
+            'id': {
+                handler: function (nv, ov) {
 
+                },
+                immediate: true,
+                deep: true
+            }
         },
         methods: {
             //跳转到指定类型页面, type:1静态试卷，2动态随机试卷
             gototype: function (type) {
-                //console.error(type);
-                let winname = window.name;
+                let typepurl = [{
+                    page: 'Modify_Type1',       //静态试卷
+                    width: 500, height: 600
+                }, {
+                    page: 'Modify_Type2',       //动态随机试卷
+                    width: 1000, height: 800
+                }];
+
+                //控制弹窗大小
                 let $pagebox = window.top.$pagebox;
-                let box = $pagebox.get(winname);
-                //box.toSize(1200,900);
-                //console.error(box);
-                box.ondrag(function (s, e) {
-                    //console.error(box.width, box.height);
-                    //console.error(e);
-                });
-                box.onmove(function (s, e) {
-                    //console.error(box.left, box.top);
-                    console.error(e);
-                });
-                box.toSize(1200, 900);
+                if ($pagebox) {
+                    let pbox = $pagebox.get(window.name);
+                    pbox.toSize(typepurl[type - 1].width, typepurl[type - 1].height);
+                }
+                //跳转到指定页面
+                let url = $api.url.set(typepurl[type - 1].page, { 'id':  this.id });
+                window.location.href = url;
             },
+            //获取试卷
+            getentity: function () {
+                var th = this;
+                if (th.isadd) return;
+                th.loadstate.get = true;
+                $api.get("ExamTestPaper/ForID", { "id": th.id })
+                    .then(req => {
+                        if (req.data.success) {
+                            th.testpaper = req.data.result;
+                            if (!th.isnull) {
+                                th.gototype(th.testpaper.Etp_Type);
+                            }
+                        } else {
+                            console.error(req.data.exception);
+                            throw req.config.way + ' ' + req.data.message;
+                        }
+                    }).catch(err => console.error(err))
+                    .finally(() => th.loadstate.get = false);
+            }
         },
         filters: {
 
