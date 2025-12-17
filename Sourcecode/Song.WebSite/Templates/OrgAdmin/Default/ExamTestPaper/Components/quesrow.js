8138776f2f9281dfede62e6c38a0b1730f39a0a8
weishakeji <10522779@qq.com>
2025-12-17 22:06:10 +0800

    编写固定试卷的试题样式，移动与移除

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Components/quesrow.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Components/quesrow.js
@@ -1,22 +1,75 @@
 
+//固定试卷的试题显示
 Vue.component('quesrow', {
-    props: ['value'],
+    //ques:试题对象
+    //index:试题的序号
+    //typeidx:试题类型的序号
+    //qtypeitems: 所有试题类型的信息
+    props: ['ques', 'index', 'typeidx', 'qtypeitems'],
     data: function () {
-        return {           
+        return {
             loading: false
         }
     },
     watch: {
-    
+        'ques': {
+            handler: function (nv, ov) {
+                //if ($api.getType(nv.Qus_Items) == "String")
+                //this.ques = window.ques.parseAnswer(nv);
+            }, immediate: true
+        }
     },
-    computed: {},
+    computed: {
+        //序号
+        serial: function () {
+            let init_idx = 0;   //初始序号
+            let idx = Number($api.clone(this.typeidx)) - 1;
+            while (idx >= 0) {
+                init_idx += this.qtypeitems[idx].ques.length;
+                idx--;
+            }
+            return init_idx + this.index + 1;
+        }
+    },
     mounted: function () {
-    
+
     },
-    methods: {       
-    
+    methods: {
+        //移动题型顺序
+        quesmove: function (index, direction) {
+            //console.error(index);
+            let item = this.qtypeitems[this.typeidx];
+            const newArr = [...item.ques];
+            const element = newArr.splice(index, 1)[0];
+            newArr.splice(index + direction, 0, element);
+            item.ques = newArr;
+        },
+        quesdelete: function (index) {
+            let item = this.qtypeitems[this.typeidx];
+            item.ques.splice(index, 1);
+        }
     },
-    template: `<div>
-        
-    </div> `
+    template: `<div class="quesrow" :qid="ques.Qus_ID">
+    <div class="queshead">
+        <div class="serial">{{serial}}.</div> 
+        <span class="btns">
+            <el-link icon="el-icon-arrow-up" type="primary" :disabled="index==0"
+                @click="quesmove(index,-1)">上移</el-link>
+            <el-link icon="el-icon-arrow-down" type="primary" :disabled="index>=qtypeitems[typeidx].ques.length-1"
+                @click="quesmove(index,1)">下移</el-link>
+            <el-popconfirm title="是否移除该试题？"  @confirm="quesdelete(index)">
+                <el-link icon="el-icon-delete" slot="reference" type="warning">移除</el-link>
+            </el-popconfirm>            
+        </span>
+    </div>
+    <div class="quescontent">
+        <div v-html="ques.Qus_Title" class="title"></div>
+        <div v-if="ques.Qus_Type==1 || ques.Qus_Type==2" class="qus_items" remark="单选、多选的选项">
+            <div v-for="(item,index) in ques.Qus_Items" :correct="item.Ans_IsCorrect"
+                v-html="String.fromCharCode(65 + index) +'.'+item.Ans_Context">
+            </div>
+        </div>
+      
+    </div>
+</div> `
 });
