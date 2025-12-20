82f5efc58bf52926dbf6b4f79fecf9295836d1d4
weishakeji <10522779@qq.com>
2025-12-20 15:45:51 +0800

    优化：试题分数的行数，可以设置

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Components/quesscores.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/ExamTestPaper/Components/quesscores.js
@@ -3,7 +3,8 @@ Vue.component('quesscores', {
     //item:试题类型的配置项
     //typeidx:试题类型的序号
     //qtypeitems: 所有试题类型的信息
-    props: ['item', 'typeidx', 'qtypeitems'],
+    //rowcount: 每行显示的试题数量
+    props: ['item', 'typeidx', 'qtypeitems', 'rowcount'],
     data: function () {
         return {
             list: [],
@@ -14,7 +15,7 @@ Vue.component('quesscores', {
         'item': {
             handler: function (val) {
                 let [count, number] = [val.count, val.number];
-                if (count == 0) count = val.ques==null ? 0 : val.ques.length;
+                if (count == 0) count = val.ques == null ? 0 : val.ques.length;
                 this.list = [];
                 if (count == 0) this.list = [];
                 else {
@@ -34,8 +35,10 @@ Vue.component('quesscores', {
         }
     },
     computed: {
-
-        'data': t => [t.count, t.number]
+        //临时数据
+        'data': t => [t.count, t.number],
+        //每行显示的数量
+        'row_count': t => t.rowcount == null ? 4 : t.rowcount,
     },
     mounted: function () {
 
@@ -51,9 +54,16 @@ Vue.component('quesscores', {
             }
             return init_idx + index + 1;
         },
+        //元素的样式
+        divstyle: function (idx) {
+            let style = 'flex: 0 0 calc(' + (100 / this.row_count) + '% - 10px);';
+            let margin = 'margin-right:0px;';
+            style += (idx+1) % this.row_count == 0 ? margin : '';
+            return style;
+        },
     },
     template: `<<div class="quesscores">
-        <div v-for="(num,idx) in list" :zero="num<=0">
+        <div v-for="(num,idx) in list" :zero="num<=0" :style="divstyle(idx)">
             <span>{{serial(idx)}}</span>
             <span :zero="num<=0"><b>{{num}}</b> 分</span>
         </div>
